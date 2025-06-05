0c45f265ba1c72de140e6cddfcae07a3df50557e
pawan_prudhvi <pawanprudhvi88@gmail.com>
2025-06-05 22:24:30 +0530

    added logprint

--- src/main/java/FoodHub/Hub/Controller/UserController.java
+++ src/main/java/FoodHub/Hub/Controller/UserController.java
@@ -117,7 +117,7 @@ public class UserController {
     @PostMapping("/verifyadmin")
     public AdminAuthResponse verifyadmin(@RequestBody Admin admin, HttpServletRequest request, HttpServletResponse response) throws Exception {
         AdminAuthResponse authresponse=userserviceimpl.authenticateAdmin(admin,response);
-        System.out.print("pawan");
+        System.out.print("pawana");
         return authresponse;
 
 
